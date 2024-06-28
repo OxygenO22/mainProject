36edd58cf751cc0e9b4af059eb33ff0e319840a5
OxygenO22 <bamaik@mail.ru>
2024-06-28 15:42:14 +0300

    Sprint 2 Extra lesson 2 is done

--- src/components/pages/s2eLection2/S2eLection2.tsx
+++ src/components/pages/s2eLection2/S2eLection2.tsx
@@ -17,10 +17,8 @@ export type TasksType = {
 
 export type FilterValuesType = "all" | "active" | "completed";
 
-
 export const S2eLection2 = () => {
 
-  
   // let todolistId1 = v1();
     // let todolistId2 = v1();
     //
@@ -160,207 +158,196 @@ export const S2eLection2 = () => {
   //     }
   // ]
 
-    const [todoFromServer, setTodoFromServer]=useState<ObjectType[]>([
-        {
-            todolistId: v1(),
-            title: "What to learn",
-            filter: "all",
-            tasks: [
-                {taskId: v1(), title: "HTML&CSS", isDone: true},
-                {taskId: v1(), title: "JS", isDone: true}
-            ],
-            students: [
-                'Rick Kane',
-                'Finnlay Bentley',
-                'Samia North',
-                'Isaac Morton',
-                'Lily-Ann Clifford',
-                'Thalia Park',
-                'Sapphire Cruz',
-                'Cieran Vazquez',
-                'Anya Estes',
-                'Dominika Field',
-                'Rosanna Chung',
-                'Safiyah Davey',
-                'Ryley Beasley',
-                'Kalvin Trejo',
-                'Evie-Mae Farrell',
-                'Juliet Valencia',
-                'Astrid Austin',
-                'Lyle Montgomery',
-                'Nisha Mora',
-                'Kylie Callaghan',
-                'Star Wilks',
-                'Marissa Colley',
-                'Asa Fuller',
-                'Leigh Kemp',
-                'Avleen Dawson',
-                'Sammy Bonilla',
-                'Acacia Becker',
-                'Coral Shepherd',
-                'Melina Molina',
-                'Kiran Bailey',
-                'Clara Escobar',
-                'Alexandru Horn',
-                'Brandon-Lee Mercado',
-                'Elouise Weston',
-                'King Long',
-                'Kerri Searle',
-                'Kanye Hamer',
-                'Elwood Benitez',
-                'Mikail Whitaker',
-                'Bobby Hardy',
-                'Talha Ferry',
-                'Priscilla Landry',
-                'Olivia-Grace Cain',
-                'Kiaan Wallace',
-                'Wesley Padilla90',
-                'Ella-Grace Wooten91',
-                'Kaif Molloy92',
-                'Kamal Broadhurst93',
-                'Bianca Ferrell94',
-                'Micheal Talbot95',
-            ]
-        },
-        {
-            todolistId:v1(),
-            title: "What to do",
-            filter: "all",
-            tasks: [
-                {taskId: v1(), title: "HTML&CSS2", isDone: true},
-                {taskId: v1(), title: "JS2", isDone: true}
-            ],
-            students: [
-                'Jago Wormald1',
-                'Saul Milne2',
-                'Aariz Hester3',
-                'Dion Reeve4',
-                'Anisa Ortega5',
-                'Blade Cisneros6',
-                'Malaikah Phelps7',
-                'Zeeshan Gallagher8',
-                'Isobella Vo9',
-                'Rizwan Mathis10',
-                'Menaal Leach11',
-                'Kian Walton12',
-                'Orion Lamb13',
-                'Faizah Huynh14',
-                'Crystal Vaughan15',
-                'Vivien Hickman16',
-                'Stuart Lu17',
-                'Karol Davison18',
-                'Dario Burns19',
-                'Chloe Rich20',
-                'Martyna Felix',
-                'Nida Glass',
-                'Maeve Miles',
-                'Hasnain Puckett',
-                'Ayman Cano',
-                'Safwan Perry',
-                'Fox Kelly',
-                'Louise Barlow',
-                'Malaki Mcgill',
-                'Leanna Cline',
-                'Willard Hodge',
-                'Amelia Dorsey',
-                'Kiah Porter',
-                'Jeanne Daly',
-                'Mohsin Armstrong',
-                'Laurie Rangel',
-                'Princess Tierney',
-                'Kasim Kendall',
-                'Darryl Cope',
-                'Elysha Ray',
-                'Liyana Harris',
-                'Kashif Blackburn',
-                'Atif Zimmerman',
-                'Sila Hartley',
-                'Ralphie Hebert',
-            ]
-        }
-    ])
-    function removeTask(id: string, todolistId: string) {
-        // //достанем нужный массив по todolistId:
-        // let todolistTasks = tasks[todolistId];
-        // // перезапишем в этом объекте массив для нужного тудулиста отфилтрованным массивом:
-        // tasks[todolistId] = todolistTasks.filter(t => t.id != id);
-        // // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
-        // setTasks({...tasks});
-        let forRemove = todoFromServer.map(
-          (task) => task.todolistId === todolistId ? {...task, tasks: task.tasks.filter(item => item.taskId !== id)} : task
-        );
+  const [todoFromServer, setTodoFromServer]=useState<ObjectType[]>([
+      {
+          todolistId: v1(),
+          title: "What to learn",
+          filter: "all",
+          tasks: [
+              {taskId: v1(), title: "HTML&CSS", isDone: true},
+              {taskId: v1(), title: "JS", isDone: true}
+          ],
+          students: [
+              'Rick Kane',
+              'Finnlay Bentley',
+              'Samia North',
+              'Isaac Morton',
+              'Lily-Ann Clifford',
+              'Thalia Park',
+              'Sapphire Cruz',
+              'Cieran Vazquez',
+              'Anya Estes',
+              'Dominika Field',
+              'Rosanna Chung',
+              'Safiyah Davey',
+              'Ryley Beasley',
+              'Kalvin Trejo',
+              'Evie-Mae Farrell',
+              'Juliet Valencia',
+              'Astrid Austin',
+              'Lyle Montgomery',
+              'Nisha Mora',
+              'Kylie Callaghan',
+              'Star Wilks',
+              'Marissa Colley',
+              'Asa Fuller',
+              'Leigh Kemp',
+              'Avleen Dawson',
+              'Sammy Bonilla',
+              'Acacia Becker',
+              'Coral Shepherd',
+              'Melina Molina',
+              'Kiran Bailey',
+              'Clara Escobar',
+              'Alexandru Horn',
+              'Brandon-Lee Mercado',
+              'Elouise Weston',
+              'King Long',
+              'Kerri Searle',
+              'Kanye Hamer',
+              'Elwood Benitez',
+              'Mikail Whitaker',
+              'Bobby Hardy',
+              'Talha Ferry',
+              'Priscilla Landry',
+              'Olivia-Grace Cain',
+              'Kiaan Wallace',
+              'Wesley Padilla90',
+              'Ella-Grace Wooten91',
+              'Kaif Molloy92',
+              'Kamal Broadhurst93',
+              'Bianca Ferrell94',
+              'Micheal Talbot95',
+          ]
+      },
+      {
+          todolistId:v1(),
+          title: "What to do",
+          filter: "all",
+          tasks: [
+              {taskId: v1(), title: "HTML&CSS2", isDone: true},
+              {taskId: v1(), title: "JS2", isDone: false}
+          ],
+          students: [
+              'Jago Wormald1',
+              'Saul Milne2',
+              'Aariz Hester3',
+              'Dion Reeve4',
+              'Anisa Ortega5',
+              'Blade Cisneros6',
+              'Malaikah Phelps7',
+              'Zeeshan Gallagher8',
+              'Isobella Vo9',
+              'Rizwan Mathis10',
+              'Menaal Leach11',
+              'Kian Walton12',
+              'Orion Lamb13',
+              'Faizah Huynh14',
+              'Crystal Vaughan15',
+              'Vivien Hickman16',
+              'Stuart Lu17',
+              'Karol Davison18',
+              'Dario Burns19',
+              'Chloe Rich20',
+              'Martyna Felix',
+              'Nida Glass',
+              'Maeve Miles',
+              'Hasnain Puckett',
+              'Ayman Cano',
+              'Safwan Perry',
+              'Fox Kelly',
+              'Louise Barlow',
+              'Malaki Mcgill',
+              'Leanna Cline',
+              'Willard Hodge',
+              'Amelia Dorsey',
+              'Kiah Porter',
+              'Jeanne Daly',
+              'Mohsin Armstrong',
+              'Laurie Rangel',
+              'Princess Tierney',
+              'Kasim Kendall',
+              'Darryl Cope',
+              'Elysha Ray',
+              'Liyana Harris',
+              'Kashif Blackburn',
+              'Atif Zimmerman',
+              'Sila Hartley',
+              'Ralphie Hebert',
+          ]
+      }
+  ])
 
-        setTodoFromServer(forRemove);
-        
-    }
-    function addTask(title: string, todolistId: string) {
-        // let task = {id: v1(), title: title, isDone: false};
-        // //достанем нужный массив по todolistId:
-        // let todolistTasks = tasks[todolistId];
-        // // перезапишем в этом объекте массив для нужного тудулиста копией, добавив в начало новую таску:
-        // tasks[todolistId] = [task, ...todolistTasks];
-        // // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
-        // setTasks({...tasks});
-    }
-    function changeStatus(id: string, isDone: boolean, todolistId: string) {
-        // //достанем нужный массив по todolistId:
-        // let todolistTasks = tasks[todolistId];
-        // // найдём нужную таску:
-        // let task = todolistTasks.find(t => t.id === id);
-        // //изменим таску, если она нашлась
-        // if (task) {
-        //     task.isDone = isDone;
-        //     // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
-        //     setTasks({...tasks});
-        // }
-    }
-    function changeFilter(value: FilterValuesType, todolistId: string) {
-        // let todolist = todolists.find(tl => tl.id === todolistId);
-        // if (todolist) {
-        //     todolist.filter = value;
-        //     setTodolists([...todolists])
-        // }
-    }
-    function removeTodolist(id: string) {
-        // // засунем в стейт список тудулистов, id которых не равны тому, который нужно выкинуть
-        // setTodolists(todolists.filter(tl => tl.id != id));
-        // // удалим таски для этого тудулиста из второго стейта, где мы храним отдельно таски
-        // delete tasks[id]; // удаляем св-во из объекта... значением которого являлся массив тасок
-        // // засетаем в стейт копию объекта, чтобы React отреагировал перерисовкой
-        // setTasks({...tasks});
-    }
 
-    return (
-      <>
-        {todoFromServer.map((tl) => {
-          let allTodolistTasks = tl.tasks;
-          let tasksForTodolist = allTodolistTasks;
-          if (tl.filter === "active") {
-            tasksForTodolist = allTodolistTasks.filter(
-              (t) => t.isDone === false
-            );
+  function removeTask(id: string, todolistId: string) {
+      let forRemove = todoFromServer.map(
+        (task) => task.todolistId === todolistId ? {...task, tasks: task.tasks.filter(item => item.taskId !== id)} : task
+      );
+
+      setTodoFromServer(forRemove);
+      
+  }
+  function addTask(title: string, todolistId: string) {
+    const newTask = {
+      taskId: v1(),
+      title,
+      isDone: false,
+    };
+
+    let newTaskToTodoList = todoFromServer.map(tl => tl.todolistId === todolistId ? {...tl, tasks: [...tl.tasks, newTask]} : tl)
+    
+    setTodoFromServer(newTaskToTodoList);
+  }
+  function changeStatus(id: string, isDone: boolean, todolistId: string) {
+    let taskStatus = todoFromServer.map((t) =>
+      t.todolistId === todolistId
+        ? {
+            ...t,
+            tasks: t.tasks.map((s) => (s.taskId === id ? { ...s, isDone } : s)),
           }
-          if (tl.filter === "completed") {
-            tasksForTodolist = allTodolistTasks.filter(
-              (t) => t.isDone === true
-            );
-          }
-
-          return (
-            <TodolistS2eLection2
-              key={tl.todolistId}
-              id={tl.todolistId}
-              title={tl.title}
-              tasks={tl.tasks}
-              removeTask={removeTask}
-              changeFilter={changeFilter}
-              addTask={addTask}
-              changeTaskStatus={changeStatus}
-              filter={tl.filter}
-              removeTodolist={removeTodolist}
-            />
-          );
-        })}
-      </>
+        : t
     );
+
+    setTodoFromServer(taskStatus);
+  }
+
+  function changeFilter(value: FilterValuesType, todolistId: string) {
+    let filteredTodoList = todoFromServer.map((tl) =>
+      todolistId === tl.todolistId ? { ...tl, filter: value } : tl
+    );
+    setTodoFromServer(filteredTodoList);
+  }
+
+  function removeTodolist(id: string) {
+      setTodoFromServer(todoFromServer.filter(tl => tl.todolistId !== id));
+  }
+
+  return (
+    <>
+      {todoFromServer.map((tl) => {
+        let tasksForTodolist = tl.tasks;
+        if (tl.filter === "active") {
+          tasksForTodolist = tl.tasks.filter((t) => !t.isDone);
+        }
+        if (tl.filter === "completed") {
+          tasksForTodolist = tl.tasks.filter((t) => t.isDone);
+        }
+
+        return (
+          <TodolistS2eLection2
+            key={tl.todolistId}
+            id={tl.todolistId}
+            title={tl.title}
+            tasks={tasksForTodolist}
+            removeTask={removeTask}
+            changeFilter={changeFilter}
+            addTask={addTask}
+            changeTaskStatus={changeStatus}
+            filter={tl.filter}
+            removeTodolist={removeTodolist}
+          />
+        );
+      })}
+    </>
+  );
 }
